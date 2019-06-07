6cd23f83088bc619903c15a913777547decfe152
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-06-07 11:29:57 +0100

    used styled component to syle header and topBar

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -13,20 +13,84 @@ const TopBar = () => {
     position: fixed;
     height: 44px;
     background-color: #333;
+`
+ const Container =styled.div`
+      width: 100%;
+      display: flex;
+      justify-content: none;
+      align-items: none;
+      flex-direction: row;
+      color: #fff;
+      letter-spacing: 1px;
+      padding: 0 10px;
+
+      @media (min-width: 1280px) {
+        width: 1280px;
+      }
+    `
+
+  const LeftContainer = styled.div`
+      display: flex;
+      justify-content: none;
+      align-items: center;
+      flex-direction: row;
+      flex: 1;
+      font-size: 11px;
+      span {
+        cursor: pointer;
+        margin-right: 25%;
+        font-weight: bold;
+      }
   `
+
+const CenterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+
+  span {
+    cursor: pointer;
+    margin-right: 5%;
+  }
+
+  span:last-child {
+    margin-right: 0;
+  }
+
+  span:hover {
+    text-decoration: underline;
+  }
+`
+
+const RightContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+
+  span {
+  cursor: pointer;
+}
+`
   return (
     <TopBar>
-      <div className="container">
-        <div className="container-left">
+      <Container>
+        <LeftContainer>
           <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
+        </LeftContainer>
+        <CenterContainer>
           <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
+        </CenterContainer>
+        <RightContainer>
           <span>LOG IN</span>
-        </div>
-      </div>
+        </RightContainer>
+      </Container>
     </TopBar>
   )
 }
